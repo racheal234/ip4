949eca04f109fe66e0034dd1bf416602e18820db
Racheal Mbugua <rachmbugua@gmail.com>
2017-11-07 13:58:37 +0300

    add content to the read me file

---
+++ README.md
@@ -0,0 +1,35 @@
+# PIG DICE GAME
+
+#### Pig Dice Game, 7th November 2017
+
+#### By Racheal Mbugua
+
+## Description
+
+Ping pong is a game played by two players where they roll a dice individually respective of their turn. The app is a fun game that is used for relaxation. If a player rolls a 1 they points are reduced to zero and the next player gets a chance to play. If one player gets to 100 then he/she is claimed as the winner and the game is over.
+
+## Setup/Installation Requirements
+
+* This is a great place
+* to list setup instructions
+* in a simple
+* easy-to-understand
+* format
+
+{Leave nothing to chance! You want it to be easy for potential users, employers and collaborators to run your app. Do I need to run a server? Do I need to install any dependencies? How should I set up my databases? Is there other code this app depends on?}
+
+## Known Bugs
+
+
+## Technologies Used
+HTML, CSS, JAVASCRIPT
+
+## Support and contact details
+
+{Let people know what to do if they run into any issues or have questions, ideas or concerns.  Encourage them to contact you or make a contribution to the code.}
+
+### License
+
+*{Determine the license under which this application can be used.  See below for more details on licensing.}*
+
+Copyright (c) {year} **Racheal Mbugua**
--- js/scripts.js
+++ js/scripts.js
@@ -1,138 +1,36 @@
-function player(Name,turn,total) {
-  this.Name = Name;
-  this.turn= turn;
-  this.total=total;
+//back-end logic
+function Player(Name, turnAmmount, totalAmmount) {
+    this.Name = Name;
+    this.turnAmmount = turnAmmount;
+    this.totalAmmount = totalAmmount;
 }
 
-
-Game.prototype.endGame = function() {
-  this.gameOver = true;
-  this.currentPlayer = 0;
-  this.players=[];
+Player.prototype.roll = function () {
+    var ranPigAmmount = (Math.floor(Math.random() * 6) + 1);
+    //    console.log(dieRanVal)
+    if (ranPigAmmount === 1) {
+        this.turnAmmount = 0;
+        // alert("Ooops! You rolled a 1. It's  " + this.Name +"'s turn");
+        dieRanVal= 0;
+    } else {
+        this.turnAmmount = this.turnAmmount + ranPigAmmount;
+        //        console.log(this.turnScore)
+    }
+    return ranPigAmmount;
 }
 
-Game.prototype.addPlayer=function(player) {
-  this.players.push(player);
+Player.prototype.ammount = function () {
+    this.totalAmmount += this.turnAmmount
+    this.turnAmmount = 0
+
+    if(this.totalAmmount >= 100){
+        alert("GAme Over");
+    }else{
+        alert("your turn is over");
+    }
 }
 
-Game.prototype.startGame=function() {
-  this.gameOver = false;
-  this.currentPlayer = 0;
+function Clearfield() {
+    $("#player1").val("");
+    $("#player2").val("");
 }
-
-Game.prototype.endTurn = function() {
-  this.players[this.currentPlayer].gameScore+=this.players[this.currentPlayer].turnScore;
-  this.players[this.currentPlayer].turnScore = 0;
-  if (this.currentPlayer<this.players.length-1) {
-    this.currentPlayer++;
-  } else {
-    this.currentPlayer = 0;
-  }
-}
-
-function Player(name) {
-  this.name=name;
-  this.gameScore=0;
-  this.turnScore=0;
-}
-
-function Die(sides) {
-  this.sides = sides;
-  this.value = 1;
-  this.roll();
-}
-
-Die.prototype.roll=function(){
-    min = 1;
-    max = this.sides;
-    this.value = Math.floor(Math.random()*(max - min+1)) + min;
-}
-
-// UI logic
-var newGame = new Game();
-
-function endTurn() {
-  newGame.endTurn();
-}
-
-function endGame() {
-  if(newGame.players[newGame.currentPlayer].gameScore + newGame.players[newGame.currentPlayer].turnScore >= 100) {
-    alert("GAME OVER: "+ newGame.players[newGame.currentPlayer].name +" won with a score of " + newGame.players[newGame.currentPlayer].gameScore + newGame.players[newGame.currentPlayer].turnScore + "!!");
-  } else {
-    alert("end game!");
-  }
-  newGame.endGame();
-  $("#listPlayers").empty();
-  $("#displayDie").html("<img src='img/roll.png' class='img-responsive'>");
-  $("#currentScore").text("Current Player Name");
-  $("#currentPlayer").text("Current Turn Score");
-  $("#hold").prop("disabled", false);
-  $("#roll").prop("disabled", false);
-  $("#playGame").prop("disabled", false);
-}
-
-function showScores() {
-  $("#listPlayerScores").empty();
-  newGame.players.forEach(function(player){
-    $("#listPlayerScores").append("<h3>"+player.name+": "+player.gameScore+"</h3>");
-  });
-}
-
-function startTurn () {
-  newGame.die.roll();
-  if(newGame.die.value === 1) {
-    newGame.players[newGame.currentPlayer].turnScore=0;
-  } else {
-    newGame.players[newGame.currentPlayer].turnScore+=newGame.die.value;
-  }
-  $("#displayDie").html("<img src='img/"+newGame.die.value+".jpg' class='img-responsive'>");
-  $("#currentScore").text(newGame.players[newGame.currentPlayer].turnScore);
-  $("#currentPlayer").text(newGame.players[newGame.currentPlayer].name + ": " + newGame.players[newGame.currentPlayer].gameScore);
-  showScores();
-  if(newGame.players[newGame.currentPlayer].turnScore===0) {
-    setTimeout(function() {
-      alert("Sorry, " + newGame.players[newGame.currentPlayer].name + " you rolled a 1. You get NOTHING!!");
-    },500);
-
-    endTurn();
-    startTurn();
-  }
-  if (newGame.players[newGame.currentPlayer].gameScore + newGame.players[newGame.currentPlayer].turnScore >= 100) {
-    setTimeout(endGame,500);
-  }
-}
-
-$("#addPlayer").click(function() {
-  var newPlayer = new Player($("#playerName").val());
-  $("#listPlayers").append("<li>" + newPlayer.name + "</li>");
-  newGame.addPlayer(newPlayer);
-  $("#playerName").val("");
-  if($("#playGame:disabled")) {
-    $("#playGame").prop("disabled", false);
-  }
-});
-
-$("#playGame").click(function(){
-  if(!newGame.gameOver) {
-    endGame();
-  }
-  newGame.startGame();
-  $("#listPlayers").hide();
-  if($("#roll:disabled")) {
-    $("#roll").prop("disabled", false);
-  }
-  if($("#hold:disabled")) {
-    $("#hold").prop("disabled", false);
-  }
-  showScores();
-  startTurn();
-});
-
-$("#roll").click(function(){
-  startTurn();
-});
-
-$("#hold").click(function(){
-  endTurn();
-  startTurn();
-});
